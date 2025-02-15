f378e50e4dc0bdc506dd38028f0e462784f71426
Samruddhi <samruddhi.admane@gmail.com>
2025-02-15 23:46:25 +0530

    reCaptcha Validation

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");  //to access html elements in javascript
+let captchchecked = false;  //we want user to submit only after he does captch check
+function beforesubmit(event){
+    if(captchchecked){
+        let outputdate = document.querySelector(".outputdate");  //to access html elements in javascript
     let inputdate = document.querySelector(".inputdate");
     console.log('inputdate.value',inputdate.value);    //string -> Date(en_IN)
 
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN"); //Locale Date according to Salesforce 
     outputdate.value = formattedDate;
+    } else{
+        alert("Please check the reCAPTCHA box to submit the Lead");
+        event.preventDefault();    //to stop the submition 
+    }
+    
 }
 
 //for now the form is on local server. TO host is over web we are using GITHUB
@@ -18,3 +25,7 @@ function timestamp() {
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
     } }
      setInterval(timestamp, 500);
+
+     function captchsuccess(){
+        captchchecked =true;
+     }
