de34b86570be40354355116aa4f2a5e035a23f37
Samruddhi <samruddhi.admane@gmail.com>
2025-02-15 23:33:37 +0530

    Recaptcha changes

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function beforesubmit(){
 
 //for now the form is on local server. TO host is over web we are using GITHUB
 //must have to github account and it is connected to your visualstudio code.
+
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    } }
+     setInterval(timestamp, 500);
